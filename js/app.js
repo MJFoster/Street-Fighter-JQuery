2b422adbdd2640bb1844b465233d9870511d3ae4
MaryJo Foster <MJ@MaryJoFoster.com>
2015-11-10 00:28:25 -0700

    Added Hadouken voice & 'How-To' text. Renamed main.html to index.html

--- js/app.js
+++ js/app.js
@@ -2,80 +2,78 @@
 // JS & Jquery code for ...
 //    Street-Fighter-JQuery webpage.
 // *************************************************
-
-// Code executes after HTML is completely loaded
 $(document).ready(function() {
 	// .show() & .hide() Multiple screens display w/audio
 	// CODE NOT YET IMPLEMENTED ...
 
 	// Using mouseleave(), change pose to "ready"
-	// Using mouseleave(), change pose to "still"
-	// Use 'chaining' here to not repeat the CSS selector!
 	$('.ryu-container').mouseenter(function() {
-		console.log("Mouse Entered!");
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
+
+	// Using mouseleave(), change pose to "still"
 	.mouseleave(function() {
-		console.log("Mouse left!");
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	});
 
-	// Upon mousedown(), throw hadouken
+
+	// Play audio for Hadouken
+	function playHadouken () {
+	  $('#hadouken-sound')[0].volume = 0.5;
+	  $('#hadouken-sound')[0].load();
+	  $('#hadouken-sound')[0].play();
+	}
+
+
+	// Upon mousedown(), play audio & throw hadouken
 	$('.ryu-container').mousedown(function () {
-		console.log("Inside 'throwHadouken'");
+		
+		// Play audio
+		playHadouken();
+
+		// Display ryu-throwing image
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
+
+		
+
+		$('.hadouken').append();
 		// To animate the hadouken itself,
-		// use show(), animate(), & finish() methods!
+		// CHAIN show(), animate(), & finish() methods!
 		$('.hadouken').show().finish().animate(
 			{left: "1020px"},
 			500,
 			function() {
 				$(this).hide();  // hide() 'this' selector
-				$(this).css("left", "620px");  // style 'this' selector
+				$(this).css("left", "620px");  // style 'this' selector back to start position.
 			});
-
-		// Play HADOUKEN throwing audio
-		// CODE NOT YET IMPLEMENTED ...
 	});
+
 
 	// Upon mouseup(), start over
 	$('.ryu-container').mouseup(function () {
-		console.log("Start over from here ...");
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-
 });
 
-
-
-// Code executes AFTER above code finishes.
 // Watch for keydown() of 'x' key, then poseCool()
 // $(document).keydown(function(down-event) {
-// 	while(down-event.which == "x" || down-event.which == "X") {
-// 		// Pose "cool" with animate();
-		// Use '<audio>' to play of music.
+// 	console.log(down-event, down-event.which);
+// 	if(down-event.which == 88) {
+// 		$('.ryu-still').hide();
+// 		$('.ryu-ready').hide();
+// 		$('.ryu-throwing').hide();
+// 		$('.ryu-cool').show();
 // 	};
 // });
+
+
+// Watch for keyup(), then poseCool()
 // $(document).keyup(function(up-event) {
-	// Stop music
-	// Pose 'still' with animate();
+// 	$('.ryu-cool').hide();
+// 	$('.ryu-still').show();
 // });
-
-
-
-// WOULD THIS WORK AS AN 'animate()' function if needed?
-// 
-// EXAMPLE of call:   $('ryu-throwing').mouseup(changePos('.ryu-still',25,0));
-//
-// function changePose(pose,x,y) {
-	// convert x & y to strings with 'px' appended.
-	// xoffset = x + "px";
-	// yoffset = y + "px";
-	
-	// $(pose).show().css("pading-left",xoffset).css("padding-top",yoffset);
-// };
